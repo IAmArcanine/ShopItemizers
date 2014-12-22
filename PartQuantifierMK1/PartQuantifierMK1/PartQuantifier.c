a95e000bc9fea95de5e986a47b2c51e5deebd0ad
IAmArcanine <matteferran@gmail.com>
2014-12-22 14:49:14 -0800

    Still trying to figure this out.

---
+++ PartQuantifierMK1/PartQuantifierMK1/PartQuantifier.c
@@ -0,0 +1,14 @@
+/*
+* Program: Part Quantifier
+* Author: Matthew Ferran
+* Date Created: 12/19/2014
+*
+* Discription: This program is an inventory system for keeping track
+*			   of the number, location, and use of various parts in 
+*			   the shop.
+*
+* Version: 1
+*/
+
+#include "PartQuantifier.h"
+
